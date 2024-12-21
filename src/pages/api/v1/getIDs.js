7de4c804386b4f6dec4e5994faff1ca3502c7d37
“bodovoet” <bodo@openseam.co>
2024-12-20 23:52:43 -0800

    update

--- src/pages/api/v1/getIDs.js
+++ src/pages/api/v1/getIDs.js
@@ -6,6 +6,7 @@ export default async function handler(req, res) {
       const result = await queryDatabase(`
         SELECT device_id, label
         FROM customer_provisioning.seambit
+        LIMIT 10000
       `);
       console.log("Query results:", result);
       res.status(200).json(result);
